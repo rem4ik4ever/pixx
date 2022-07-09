963adc1506d382d26a6e91f00286a3f1d97fff16
Rem Kim <rem4ik4ever@gmail.com>
2022-07-08 20:28:16 -0400

    next-seo and animation
    Signed-off-by: Rem Kim <rem4ik4ever@gmail.com>

---
+++ src/seo/index.ts
@@ -0,0 +1,17 @@
+const defaultSeo = {
+  title: 'Email automation for your business',
+  titleTemplate: '%s - Flows',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.flows.so/',
+    site_name: 'Flows',
+  },
+  twitter: {
+    handle: '@TheRealRemKim',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+};
+
+export default defaultSeo
