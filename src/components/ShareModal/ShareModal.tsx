d2cf4e3e00c31753c510bb0c7fbd126ad757a0e9
Rem Kim <rem4ik4ever@gmail.com>
2022-11-09 16:09:06 -0500

    add rating component
    Signed-off-by: Rem Kim <rem4ik4ever@gmail.com>

--- src/components/ShareModal/ShareModal.tsx
+++ src/components/ShareModal/ShareModal.tsx
@@ -1,7 +1,6 @@
-import Image from 'next/image'
-import { VideoCameraIcon } from '@heroicons/react/24/solid'
 import s from './ShareModal.module.css'
 import dynamic from 'next/dynamic'
+import { Rating } from './Rating'
 
 const TestimonialTypeSelector = dynamic(() => import("./TestimonialTypeSelector"), { ssr: false })
 
@@ -10,7 +9,7 @@ export const ShareModal = () => {
     <div className={s.root}>
       <h1 className="text-2xl text-primary font-bold mb-4">HouseWork</h1>
       <h2 className="text-accent-6 text-center">If you are satisfied with our service please leave a testimonial, it means a lot for us!</h2>
-
+      <Rating />
       <TestimonialTypeSelector />
     </div>
   )
